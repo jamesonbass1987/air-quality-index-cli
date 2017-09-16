75f62b67fe06a23524037dc049c406ff3084dd67
Jameson Bass <jamesonbass@gmail.com>
2017-09-16 22:37:08 +0000

    updated local_aqi class methods

--- lib/air_quality_index/cli.rb
+++ lib/air_quality_index/cli.rb
@@ -13,6 +13,7 @@ class AirQualityIndex::CLI
     1. Local AQI Information
     2. Top 5 Nationwide AQI Rankings
     3. General AQI Information
+
     DOC
   end
 
@@ -21,22 +22,34 @@ class AirQualityIndex::CLI
     puts "Please enter 1-3 to make your selection, or type exit."
 
     user_input = nil
-
     while user_input != 'exit'
+
       #gets user input
-      user_input = gets.chomp
-
+      user_input = gets.strip.downcase
       #based on user input, call appropriate class method
-
-      if user_input == '1'
+      case user_input
+      when '1'
         AirQualityIndex::LocalAQI.new.call
-      elsif user_input == '2'
+        self.return_message
+      when '2'
         AirQualityIndex::NationwideAQI.new.call
-      elsif user_input == '3'
+        self.return_message
+      when '3'
         AirQualityIndex::AQI_Information.new.call
+        self.return_message
+      when 'return'
+        self.list_options
+        self.menu
       else
-        "Invalid entry. Please enter a numeric selection 1-3, or type 'exit'."
+        puts "Invalid entry. Please enter a numeric selection 1-3, or type 'exit'."
       end
     end
   end
+
+  def return_message
+    puts ""
+    puts "Type 'return' to go back to previous menu, or type 'exit'."
+    puts ""
+  end
+
 end
--- lib/air_quality_index/local_aqi.rb
+++ lib/air_quality_index/local_aqi.rb
@@ -1,9 +1,10 @@
 class AirQualityIndex::LocalAQI
 
-  attr_accessor :zip_code
+  attr_accessor :zip_code, :todays_index, :current_location, :current_aqi, :current_ozone, :current_aqi_msg, :current_ozone_msg, :current_aqi_timestamp, :today_aqi, :today_aqi_msg, :tomorrow_aqi, :tomorrow_aqi_msg, :today_ozone, :tomorrow_ozone
 
   def call
     self.zip_code_grabber
+    self.local_aqi_return(self.local_aqi_index)
   end
 
   # grab zip code from user
@@ -28,5 +29,58 @@ class AirQualityIndex::LocalAQI
     else
       user_zip.size == 5 && user_zip.to_i.to_s == user_zip && user_zip != nil
     end
+  end
+
+  def local_aqi_index
+
+    #store location information
+    self.current_location = 'Portland'
+
+    #store current aqi/ozone data
+
+    self.current_aqi = '135'
+    self.current_aqi_msg = '(Unhealthy For Sensitive Groups)'
+    self.current_ozone = '13'
+    self.current_ozone_msg = '(Good)'
+    self.current_aqi_timestamp = '10:00 PDT'
+
+    #store todays forecast data
+    self.today_aqi = 'Good'
+    self.today_aqi_msg = 'None'
+    self.today_ozone = 'Good'
+
+    #store tomorrows forecast data
+
+    self.tomorrow_aqi = 'Good'
+    self.tomorrow_aqi_msg = 'None'
+    self.tomorrow_ozone = 'Good'
+
+    #return self
+    self
+  end
+
+  def local_aqi_return(local_aqi)
+
+    puts <<-DOC
+
+    Current Conditions in #{local_aqi.current_location} (AQI Observed at #{local_aqi.current_aqi_timestamp}):
+
+    AQI - #{local_aqi.current_aqi} #{local_aqi.current_aqi_msg}
+    Ozone - #{local_aqi.current_ozone} #{local_aqi.current_ozone_msg}
+
+    Today's Forecast in #{local_aqi.current_location}
+
+    AQI - #{local_aqi.today_aqi}
+    Ozone - #{local_aqi.today_ozone}
+    Health Message - #{local_aqi.today_aqi_msg}
+
+
+    Tomorrow's Forecast in #{local_aqi.current_location}
+
+    AQI - #{local_aqi.tomorrow_aqi}
+    Ozone - #{local_aqi.tomorrow_ozone}
+    Health Message - #{local_aqi.tomorrow_aqi_msg}
+    DOC
+
   end
 end
