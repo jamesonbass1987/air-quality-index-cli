dab62d1d03b59298c88eea3c71c73ad6e70b755e
Jameson Bass <jamesonbass@gmail.com>
2017-09-18 00:40:41 +0000

    updated file structure

--- lib/air_quality_index.rb
+++ lib/air_quality_index.rb
@@ -2,9 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+
 require_relative "./air_quality_index/version"
 require_relative "./air_quality_index/cli"
 require_relative "./air_quality_index/local_aqi"
 require_relative "./air_quality_index/nationwide_aqi"
 require_relative "./air_quality_index/aqi_info"
 require_relative "./air_quality_index/scraper.rb"
+
+module AirQualityIndex
+end
